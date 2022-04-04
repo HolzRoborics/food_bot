8237a5b04d34a0f277966547ed037772446ed7d6
Nik <17rolex@gmail.com>
2022-04-04 21:51:32 +0300

    load users form excel, db models

--- bot/databases/models/__init__.py
+++ bot/databases/models/__init__.py
@@ -1,1 +1,3 @@
 from .user import User
+from .food import Food
+from .order import Order
--- bot/databases/models/user.py
+++ bot/databases/models/user.py
@@ -1,8 +1,10 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, BigInteger
 
 from .. import Base
-from .base import BaseMixin
 
 
-class User(BaseMixin, Base):
-    name = Column(String(50))
+class User(Base):
+    __tablename__ = 'user'
+    scud_id = Column(BigInteger, nullable=False, index=True, primary_key=True)
+    name = Column(String(100))
+    telegram_id = Column(BigInteger, nullable=False, index=True)
